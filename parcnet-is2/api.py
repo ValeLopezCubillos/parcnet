191bcdd968f60e0a31abaedf1399038be147a56a
ValeLopezCubillos <98857432+ValeLopezCubillos@users.noreply.github.com>
2025-05-05 19:54:03 -0500

    Condicion para pasar por parcnet

--- parcnet-is2/api.py
+++ parcnet-is2/api.py
@@ -7,8 +7,10 @@ import torch
 import tempfile
 import uvicorn
 import librosa
+import traceback
 from parcnet import PARCnet
 from io import BytesIO
+import subprocess
 
 app = FastAPI()
 
@@ -62,27 +64,44 @@ def get_note_from_freq(freq: float):
 async def detect_note(audio: UploadFile = File(...)):
     try:
         raw = await audio.read()
-        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp_audio:
-            tmp_audio.write(raw)
-            tmp_audio_path = tmp_audio.name
+        with tempfile.NamedTemporaryFile(suffix=".webm", delete=False) as tmp_webm:
+            tmp_webm.write(raw)
+            tmp_webm_path = tmp_webm.name
 
-        signal, sr = sf.read(tmp_audio_path)
+        tmp_wav = tempfile.NamedTemporaryFile(suffix=".wav", delete=False)
+        tmp_wav_path = tmp_wav.name
+        tmp_wav.close()
+
+        ffmpeg_cmd = [
+            "C:/ffmpeg/ffmpeg-7.1.1-essentials_build/bin/ffmpeg.exe", "-y",
+            "-i", tmp_webm_path,
+            "-ar", "16000",  
+            "-ac", "1",      
+            tmp_wav_path
+        ]
+        subprocess.run(ffmpeg_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+
+        signal, sr = sf.read(tmp_wav_path)
         if signal.ndim > 1:
             signal = np.mean(signal, axis=1)
         signal = signal.astype(np.float32)
 
         trace = detect_loss_trace(signal)
-        if 0 in trace:
+        loss_ratio = np.mean(trace == 0)
+        if loss_ratio > 0.01:
+            print(f"Pérdida detectada: {loss_ratio*100:.2f}%")
+            print("Ejecutando PARCnet para reconstrucción...")
             signal = parcnet(signal, trace)
 
         pitches = librosa.yin(signal, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'), sr=sr)
         freq = float(np.median(pitches))  
-
         note = get_note_from_freq(freq)
 
         return JSONResponse(content={"note": note, "frequency": freq})
 
     except Exception as e:
+        print("❌ Error en /detect_note:", str(e))
+        traceback.print_exc()
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 @app.post("/parcnet")
